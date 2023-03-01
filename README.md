f08b8c08bc740c3eb9bb455ef116cf44432b572f
ilovebgp4 <ilovebgp4@gmail.com>
2023-03-01 19:48:27 +0800

    Update and rename README.md to README.s

--- README.md
+++
@@ -1,2 +0,0 @@
-# script practise to simplify daily work
-## automat send batchmail to receipts in excel and do little contents change
---
+++ README.s
@@ -0,0 +1,2 @@
+# This script is a practise to simplify daily work
+## Automate send batchmail to recipients read from excel and adjusted contents littlely
